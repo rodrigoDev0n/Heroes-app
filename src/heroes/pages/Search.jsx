a1f96cc571f3066958571596ed649434e2bce5c2
Rodrigo Poblete <pobletedev@gmail.com>
2022-12-28 01:34:41 -0300

    añadiendo ultimos cambios

--- src/heroes/pages/Search.jsx
+++ src/heroes/pages/Search.jsx
@@ -1,8 +1,69 @@
+import queryString from 'query-string'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useForm } from '../../hooks/useForm'
+import {HeroeCard} from '../components/index'
+import { getHeroByName } from '../helpers/getHeroByname'
 
 export const Search = () => {
+
+  const {searchText, onInputChange, onResetForm} = useForm({
+    'searchText': '',
+  }) 
+
+  
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const {q = ''} = queryString.parse(location.search)
+  const heroe = getHeroByName(q)
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (searchText.trim().length <= 1) return
+    navigate(`?q=${searchText.toLowerCase().trim()}`)
+    onResetForm()
+  }
+
   return (
     <>
-        <h1>Search Page</h1>
+        <h1>Search - Busquedas</h1>
+        <hr/>
+        <div className="row">
+           <div className="col-5">
+             <h1>Search</h1>
+             <hr/>
+             <form onSubmit={handleSubmit}>
+               <input 
+                  type="text" 
+                  name="searchText" 
+                  value={searchText} 
+                  onChange={onInputChange} 
+                  autoComplete="of" 
+                  className="form-control"
+                />
+               <button type="" className="btn btn-primary mt-3">Search a Hero</button>
+             </form>
+           </div>
+           <div className="col-7">
+             <h1>Result</h1>
+             <hr/>
+             {
+               q === '' 
+               ? <div className='alert alert-primary animate__animated animate__bounce'>
+                  Busca un heroe
+                </div>
+               : (heroe.length === 0) 
+               && <div className='alert alert-danger animate__animated animate__bounce'>
+                  No hay heroes con el nombre <b>{q}</b>
+                 </div>
+             }
+             {
+                heroe.map(e => (
+                 <HeroeCard key={e.id} heroe={e} />
+                ))
+             }
+           </div>
+        </div>
     </>
   )
 }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,17 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom"
-
 import { Login } from "../auth/auth"
 import { DC, Marvel, AppBar } from '../heroes/heroes'
 import { HeroesRoutes } from "../heroes/routes/HeroesRoutes"
 
-const hoole = "Hola"
-
 export const AppRouter = () => {
     return (
         <>
-            <h1>Rutas</h1>
-            <hr/>
-      
             <Routes>
                 <Route path="/login" element={ <Login /> } />
                 <Route path="/*" element={ <HeroesRoutes /> } />
